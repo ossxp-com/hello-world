75346b3283da5d8117f3fe66815f8aaaf5387321
Jiang Xin <jiangxin@ossxp.com>
2010-12-31 11:04:44 +0800

    Hello world initialized.
    Signed-off-by: Jiang Xin <jiangxin@ossxp.com>

---
+++ src/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int usage(int code)
+{
+    printf("Hello world example.\n"
+           "Copyright Jiang Xin <jiangxin AT ossxp DOT com>, 2009\n"
+           "\n"
+           "Usage:\n"
+           "    hello\n"
+           "            say hello to the world.\n\n"
+           "    hello <username>\n"
+           "            say hi to the user.\n\n"
+           "    hello -h, -help\n"
+           "            this help screen.\n\n");
+    return code;
+}
+
+int
+main(int argc, char **argv)
+{
+    if (argc == 1) {
+        printf ("Hello world.\n");
+    } else if ( strcmp(argv[1],"-h") == 0 ||
+                strcmp(argv[1],"--help") == 0 ) {
+                return usage(0);
+    } else {
+        printf ("Hi, %s.\n", argv[1]);
+    }
+}
