08312489d4136223e7f4923e40cb487783d4a005
Jiang Xin <jiangxin@ossxp.com>
2010-12-31 12:08:43 +0800

    Add I18N support.
    Signed-off-by: Jiang Xin <jiangxin@ossxp.com>

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include "version.h"
 
+#include <locale.h>
+#include <libintl.h>
+#define _(String) gettext (String)
+#define gettext_noop(String) String
+#define N_(String) gettext_noop (String)
+
 int usage(int code)
 {
-    printf("Hello world example %s\n"
+    printf(_("Hello world example %s\n"
            "Copyright Jiang Xin <jiangxin AT ossxp DOT com>, 2009.\n"
            "\n"
            "Usage:\n"
@@ -12,20 +18,24 @@ int usage(int code)
            "    hello <username>\n"
            "            say hi to the user.\n\n"
            "    hello -h, -help\n"
-           "            this help screen.\n\n", _VERSION);
+           "            this help screen.\n\n"), _VERSION);
     return code;
 }
 
 int
 main(int argc, char **argv)
 {
+    setlocale( LC_ALL, "" );
+    bindtextdomain("helloworld","locale");
+    textdomain("helloworld");
+
     if (argc == 1) {
-        printf ("Hello world.\n");
+        printf ( _("Hello world.\n") );
     } else if ( strcmp(argv[1],"-h") == 0 ||
                 strcmp(argv[1],"--help") == 0 ) {
                 return usage(0);
     } else {
-        printf ("Hi, %s.\n", argv[1]);
+        printf (_("Hi, %s.\n"), argv[1]);
     }
 
     printf( "(version: %s)\n", _VERSION );
